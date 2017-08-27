38da88cf1db22b86c5ae73e6477d77c20f845b16
Julian Adams <jadams@espol.edu.ec>
2017-08-27 05:56:58 -0500

    Compilando aplicacion y añadiendo icono

--- www/js/app.js
+++ www/js/app.js
@@ -4,157 +4,177 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic-material', 'ionMdInput'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic-material'])
 
 .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-            /*StatusBar.overlaysWebView(false);
-            StatusBar.backgroundColorByHexString('#f2aa00');*/
-        }
-    });
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
 })
 
 .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
-    // Titulo de las vistas en el centro
-    $ionicConfigProvider.navBar.alignTitle('center');
+  // Titulo de las vistas en el centro
+  $ionicConfigProvider.navBar.alignTitle('center');
 
-    // Turn off caching for demo simplicity's sake
-    $ionicConfigProvider.views.maxCache(0);
+  // Turn off caching for demo simplicity's sake
+  $ionicConfigProvider.views.maxCache(0);
 
-    /*
-    // Turn off back button text
-    $ionicConfigProvider.backButton.previousTitleText(false);
-    */
+  /*
+  // Turn off back button text
+  $ionicConfigProvider.backButton.previousTitleText(false);
+  */
 
-    $stateProvider.state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/login/initLogin.html',
-        controller: 'InitLoginCtrl'
-    })
+  $stateProvider.state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/login/initLogin.html',
+    controller: 'InitLoginCtrl'
+  })
+  
+  /*////////////////////
+      Login 
+  ////////////////////*/
+  .state('app.login', {
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login/login.html',
+        controller: 'LoginCtrl'
+      }
+    }
+  })
+  
+  
+  /*/////////////////////////////////////////////////
+          Estados y vistas para pacientes
+  /////////////////////////////////////////////////*/
 
-    // Login 
-    .state('app.login', {
-        url: '/login',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/login/login.html',
-                controller: 'LoginCtrl'
-            },
-            'fabContent': {
-                template: ''
-            }
-        }
-    })
 
-    /* ********************************************* */
-    /*/////////////////////////////////////////////////
-        Estados y vistas para niños
-    /////////////////////////////////////////////////*/
-    .state('paciente', {
-        url: '/paciente',
-        abstract: true,
-        templateUrl: 'templates/paciente/menuChild.html',
-        controller: 'AppCtrl'
-    })
+  /*////////////////////
+      Menú lateral
+  ////////////////////*/
+  .state('paciente', {
+    url: '/paciente',
+    abstract: true,
+    templateUrl: 'templates/paciente/menuChild.html',
+    controller: 'AppCtrl'
+  })
 
-    // Pagina principal 
-    .state('paciente.profile', {
-        url: '/profile',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/paciente/profileChild.html',
-                controller: 'ProfileCtrl'
-            },
-            'fabContent': ''
-        }
-    })
+   
+  /*//////////////////////
+      Página principal
+  //////////////////////*/
+  .state('paciente.profile', {
+    url: '/profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/paciente/profileChild.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
 
-    // Formulario datos del perfil 
-    .state('paciente.editProfile', {
-        url: '/editarPerfil',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/paciente/formPerfil.html',
-                controller: 'ProfileCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fabSubProf" class="button button-fab button-fab-bottom-right theme-color-app" ng-click="submitProfile()"><i class="icon ion-checkmark"></i></button>',
-                controller: 'ProfileCtrl'
-            }
-        }
-    })
 
-    // Centros médicos 
-    .state('paciente.centros', {
-        url: '/centros',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/paciente/centro.html',
-                controller: 'CentrosCtrl'
-            },
-            'fabContent': ''
-        }
-    })
+  /*//////////////////////////////////
+      Formulario datos del perfil 
+  //////////////////////////////////*/
+  .state('paciente.editProfile', {
+    url: '/editarPerfil',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/paciente/formPerfil.html',
+        controller: 'ProfileCtrl'
+      },
+      'fabContent': {
+        template: '<button id="fabSubProf" class="button button-fab button-fab-bottom-right theme-color-app" ng-click="submitProfile()"><i class="icon ion-checkmark"></i></button>',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
 
-    // Datos de control 
-    .state('paciente.control', {
-        url: '/control',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/paciente/datosControl.html',
-                controller: 'ControlCtrl'
-            },
-            'fabContent': ''
-        }
-    })
+    
+  /*//////////////////////////////////
+      Centros médicos 
+  //////////////////////////////////*/
+  .state('paciente.centros', {
+    url: '/centros',
+    views: {
+     'menuContent': {
+        templateUrl: 'templates/paciente/centro.html',
+        controller: 'CentrosCtrl'
+      }
+    }
+  })
+  
+  
+  /*//////////////////////////////////
+      Datos de control 
+  //////////////////////////////////*/
+  .state('paciente.control', {
+    url: '/control',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/paciente/datosControl.html',
+        controller: 'ControlCtrl'
+      }
+    }
+  })
 
-    // Plan nutricional 
-    .state('paciente.plan', {
-        url: '/plan',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/paciente/plan.html',
-                controller: 'PlanCtrl'
-            },
-            'fabContent': ''
-        }
-    })
+  
+  /*//////////////////////////////////
+      Plan nutricional 
+  //////////////////////////////////*/
+  .state('paciente.plan', {
+    url: '/plan',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/paciente/plan.html',
+        controller: 'PlanCtrl'
+      }
+    }
+  })
 
-    // Citas 
-    .state('paciente.citas', {
-        url: '/citas',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/paciente/citas.html',
-                controller: 'CitasCtrl'
-            },
-            'fabContent': ''
-        }
-    })
+  
+  /*//////////////////////////////////
+      Citas 
+  //////////////////////////////////*/
+  .state('paciente.citas', {
+    url: '/citas',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/paciente/citas.html',
+        controller: 'CitasCtrl'
+      }
+    }
+  })
 
-    // Estadisticas 
-    .state('paciente.logros', {
-        url: '/logros',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/paciente/logros.html',
-                controller: 'LogrosCtrl'
-            },
-            'fabContent': ''
-        }
-    })
-    ;
+  
+  /*//////////////////////////////////
+      Estadisticas 
+  //////////////////////////////////*/
+  .state('paciente.logros', {
+    url: '/logros',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/paciente/logros.html',
+        controller: 'LogrosCtrl'
+      }
+    }
+  })
+  ;
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/login');
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/login');
+  
 });
